0c67091b39428d5a919f2c0d9087cc1b6fb8ff00
Utkarsh <103994909+UtkarshOnGitHub@users.noreply.github.com>
2022-12-15 10:52:49 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 const dbConnect = ()=>{
     mongoose.set('strictQuery', true);
-    return mongoose.connect('mongodb+srv://emi:emi@cluster0.r1efyyx.mongodb.net/emi');
+    return mongoose.connect('mongodb+srv://kira:kira@cluster0.0iwcmem.mongodb.net/mocktest');
 }
 module.exports = dbConnect;
